e5e0f87bff515488939bbc28bbd0035b8bd83c3b
BADGUY0592 <thebadguyisbck@gmail.com>
2020-07-30 22:20:27 +0530

    bug fixes

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,7 +1,7 @@
 """todolist URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #Auth
+    path('signup/', views.signupuser,name="signupuser"),
 ]
